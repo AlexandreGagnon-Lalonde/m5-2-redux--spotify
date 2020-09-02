cee00514c4a532fe113fbcbfca919898f13dfb6d
AlexandreGagnon-Lalonde <alexandre.gl@hotmail.ca>
2020-09-02 16:00:32 -0400

    route initiation in app.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,28 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 import GlobalStyles from "./GlobalStyles";
+
+import ArtistRoute from "../ArtistRoute";
+
+const DEFAULT_ARTIST_ID = "5j4HeCoUlzhfWtjAfM1acR";
 
 const App = () => {
   return (
     <>
       <GlobalStyles />
-      <Router>TODO</Router>
+      <Router>
+        <Route path="/artist/:id">
+          <ArtistRoute />
+        </Route>
+        <Route exact path="/">
+          <Redirect to={`/artist/${DEFAULT_ARTIST_ID}`} />
+        </Route>
+      </Router>
     </>
   );
 };
---
+++ src/components/ArtistRoute/ArtistRoute.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class ArtistRoute extends Component {
+  render() {
+    return (
+      <>
+        Hello
+      </>
+      );
+  }
+}
+
+export default ArtistRoute;
