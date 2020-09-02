57d972326da48a03d6aced645b44713f8da51b4f
AlexandreGagnon-Lalonde <alexandre.gl@hotmail.ca>
2020-09-02 11:36:09 -0400

    token access

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,14 @@
-import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import GlobalStyles from './GlobalStyles';
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import GlobalStyles from "./GlobalStyles";
 
 const App = () => {
-  return <Router>
-    <GlobalStyles /> TODO
-  </Router>;
+  return (
+    <>
+      <GlobalStyles />
+      <Router>TODO</Router>
+    </>
+  );
 };
 
 export default App;
