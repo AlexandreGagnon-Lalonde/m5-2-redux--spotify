e5606be9231dbe1e6f0462fddd46cfe3bd39c494
AlexandreGagnon-Lalonde <alexandre.gl@hotmail.ca>
2020-09-03 17:37:45 -0400

    render thing when token is fetched

--- src/components/ArtistRoute/ArtistRoute.js
+++ src/components/ArtistRoute/ArtistRoute.js
@@ -1,8 +1,27 @@
 import React, { Component } from "react";
 import { useSelector } from "react-redux";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useParams,
+} from "react-router-dom";
+import { fetchArtistProfile } from "../../helpers/api-helpers";
 
 const ArtistRoute = () => {
   const accessToken = useSelector((state) => state.auth.token);
+
+  const artistId = useParams();
+
+  React.useEffect(() => {
+    if (!accessToken) {
+      return;
+    }
+
+    fetchArtistProfile(accessToken, artistId.id);
+  }, [accessToken]);
+
   return accessToken;
 };
 
